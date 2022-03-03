78bd777934edba25cb70445a5256d8207eef99e4
kian jones <kianjones9@gmail.com>
2022-03-03 22:41:36 +0000

    custom containers almost working

--- rackify/containers/ovs/config.sh
+++ rackify/containers/ovs/config.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# get IP to remove
+IP=$(ip add | grep -o 172.17.[0-9]*.[0-9]*/16)
+
 # OVS config:
 # remove int IPs
-ip add del 172.17.0.0/16 dev eth0
+ip add del $IP dev eth0
+ip add del $IP dev eth1
 
 # add a bridge
 ovs-vsctl add-br br0
@@ -18,8 +22,11 @@ ovs-vsctl set bridge br0 protocols=OpenFlow10,OpenFlow13
 
 # set bridge IP
 ip addr flush dev eth0
-ip addr add 172.17.10.12/24 dev br0
+ip addr add 172.17.$RACK_NUM.12/24 dev br0
 ip link set br0 up
+
+# add default route
+ip route add default dev br0
 
 #container config:
 #       enable packet forwarding?
