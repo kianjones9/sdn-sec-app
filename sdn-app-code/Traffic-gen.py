3c02e5a474805758c7a781e1e1945de2d6e62e41
Kian Jones <kianjones9@gmail.com>
2022-03-30 01:13:54 +0000

    Pushing changes to traffic generator

--- sdn-app-code/Traffic-gen.py
+++ sdn-app-code/Traffic-gen.py
@@ -1,26 +1,15 @@
+from asyncore import loop
 import sys
-from scapy.all import Ether,IP,TCP, sendpfast
+from scapy.all import Ether,IP,TCP, sendpfast, ICMP, send
 
 if sys.argv[1] == "--help":
-    print("\nCorrect formatting is:\n\tpython3 Capstone-scapy.py 'type of demo: PPS, HTTP' 'Dst IP address' 'SRC IP address'\n")
+    print("\nCorrect formatting is:\n\tpython3 Traffic-gen.py 'type of demo: PPS-1 or PPS-100' 'Dst IP address' 'SRC IP address'\n")
+    print("PPS-1: PPS = 1pkt/s\nPPS-100: PPS = 100pkt/s\n\nPackets will be sent for 10 minutes or until Ctrl-C is pressed\n")
 
-elif sys.argv[1] == "HTTP":
-    #Specify what IP you want when running the script
-    pkt = Ether()/IP(dst=sys.argv[2],src=sys.argv[3])/TCP(sport=80,dport=80)/"GET /index.html HTTP/1.0"#Demo Data"
-    #pps is how many packet you want per second, 
-    #loop is how long you want to run it with each 1 second = pps x 10
-    sendpfast(pkt, pps=1000, loop=10000)
+elif sys.argv[1] == "PPS-1":
+    #PPS = 1pkt/s for 10mins or until Ctrl-C is pressed
+    pkt=send(IP(dst=sys.argv[2],src=sys.argv[3])/ICMP(), count=600, inter=1)
 
-elif sys.argv[1] == "PPS":
-    #Specify what IP you want when running the script
-    pkt = Ether()/IP(dst=sys.argv[2],src=sys.argv[3])/TCP()/"#Demo Data"
-    #pps is how many packet you want per second, 
-    #loop is how long you want to run it with each 1 second = pps x 10
-    sendpfast(pkt, pps=1000, loop=10000)
-
-elif sys.argv[1] == "BW":
-    #Will output 500mbps to dst for 10 seconds
-    pkt = Ether()/IP(dst=sys.argv[2],src=sys.argv[3])/TCP()/"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz" 
-    # PPS 2000 = 1 mpbs
-    # loop is how long you want to run it with each 1 second = pps x 10
-    sendpfast(pkt, pps=200000, loop=2000000)
+elif sys.argv[1] == "PPS-100":
+    #PPS = 100pkt/s for 10mins or until Ctrl-C is pressed
+    pkt=send(IP(dst=sys.argv[2],src=sys.argv[3])/ICMP(), count=60000, inter=0.01)
